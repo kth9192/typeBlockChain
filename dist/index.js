7380bcf585064d9efcb350ab785db0b95a4b71bf
kth919 <kth9193@gmail.com>
2019-11-18 19:17:33 +0900

    unchased dist

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const sayHi = (name, age, gender) => {
-    return `hello ${name} , you are ${age} , and ${gender}!`;
-};
-console.log(sayHi("kim", 28, "male"));
-//# sourceMappingURL=index.js.map
